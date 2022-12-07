86ae74eeb39132d974ae8667bb556fe3bdf8bcc2
Yokesh22 <yokesh.ssmy2210@gmail.com>
2022-12-07 21:33:05 +0530

    react native folder structure commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  return(
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black"
+}
+});
